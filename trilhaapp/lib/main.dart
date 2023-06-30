18b56dd03ee4226a26b2d9a7d94e627746ab40c0
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-06-29 21:40:12 -0300

    Reorganizando codigo

--- trilhaapp/lib/main.dart
+++ trilhaapp/lib/main.dart
@@ -1,56 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+
+import 'my_app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var numeroGerado = 0;
-
-  int _gerarNumeroAleatorio() {
-    Random numeroAleatorio = Random();
-    return numeroAleatorio.nextInt(1000);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Meu App"),
-      ),
-      body: Center(child: Text(numeroGerado.toString())),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add_box),
-        onPressed: () {
-          setState(() {
-            numeroGerado = _gerarNumeroAleatorio();
-          });
-        },
-      ),
-    );
-  }
 }
