569ea033459753c2dee1e6453d9ff3eb491bfaad
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-07-04 20:08:58 -0300

    Alterando fonts

--- trilhaapp/lib/pages/home_page.dart
+++ trilhaapp/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:trilhaapp/service/gerador_numero_aleatorio_service.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,9 +16,14 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Meu App"),
+        title: Text(
+          "Meu App",
+          style: GoogleFonts.qwigley(fontSize: 40),
+        ),
       ),
-      body: Center(child: Text(numeroGerado.toString())),
+      body: Center(
+          child: Text(numeroGerado.toString(),
+              style: GoogleFonts.acme(fontSize: 20))),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add_box),
         onPressed: () {
