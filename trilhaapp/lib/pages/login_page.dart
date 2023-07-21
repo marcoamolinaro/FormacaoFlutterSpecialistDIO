cfd3b904b17b98bcecd4ee2b8f40202bdc7e203f
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-07-21 14:17:30 -0300

    Navegando para a HomePage

--- trilhaapp/lib/pages/login_page.dart
+++ trilhaapp/lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/pages/main_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -138,10 +139,10 @@ class _LoginPageState extends State<LoginPage> {
                             if (emailController.text.trim() ==
                                     "email@email.com" &&
                                 senhaController.text.trim() == "123") {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                      content:
-                                          Text("Login efetuado com sucesso!")));
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const MainPage()));
                             } else {
                               ScaffoldMessenger.of(context).showSnackBar(
                                   const SnackBar(
