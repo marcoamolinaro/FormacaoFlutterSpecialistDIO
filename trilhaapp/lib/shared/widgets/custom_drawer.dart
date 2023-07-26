86f1940cbf72ab822511809828ecbf17682c31e0
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-07-26 15:15:10 -0300

    showModalBottomSheet

--- trilhaapp/lib/shared/widgets/custom_drawer.dart
+++ trilhaapp/lib/shared/widgets/custom_drawer.dart
@@ -11,15 +11,43 @@ class CustomDrawer extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          UserAccountsDrawerHeader(
-              decoration: const BoxDecoration(color: Colors.blue),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Image.network(
-                    "https://hermes.digitalinnovation.one/assets/diome/logo.png"),
-              ),
-              accountName: const Text("Marco Aurelio"),
-              accountEmail: const Text("marco@gmail.com")),
+          InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Wrap(
+                      children: [
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: const Text("Camera"),
+                          leading: const Icon(Icons.camera),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: const Text("Galeria"),
+                          leading: const Icon(Icons.album),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            child: UserAccountsDrawerHeader(
+                decoration: const BoxDecoration(color: Colors.blue),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Image.network(
+                      "https://hermes.digitalinnovation.one/assets/diome/logo.png"),
+                ),
+                accountName: const Text("Marco Aurelio"),
+                accountEmail: const Text("marco@gmail.com")),
+          ),
           InkWell(
               onTap: () {
                 Navigator.pop(context);
@@ -63,7 +91,39 @@ class CustomDrawer extends StatelessWidget {
             height: 10,
           ),
           InkWell(
-              onTap: () {},
+              onTap: () {
+                showModalBottomSheet(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    context: context,
+                    builder: (BuildContext bc) {
+                      return Container(
+                        padding: const EdgeInsets.all(10),
+                        child: const Column(
+                          children: [
+                            Text(
+                              "Termos de Uso e Privacidade",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "Caros amigos, o novo modelo estrutural aqui preconizado nos obriga à análise dos níveis de motivação departamental. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que o julgamento imparcial das eventualidades promove a alavancagem dos índices pretendidos. O incentivo ao avanço tecnológico, assim como a complexidade dos estudos efetuados acarreta um processo de reformulação e modernização das formas de ação. Não obstante, a estrutura atual da organização facilita a criação do levantamento das variáveis envolvidas.",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ],
+                        ),
+                      );
+                    });
+              },
               child: Container(
                   padding: const EdgeInsets.symmetric(horizontal: 5),
                   width: double.infinity,
