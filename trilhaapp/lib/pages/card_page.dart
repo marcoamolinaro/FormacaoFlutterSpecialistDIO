d10939a6282351d6af57acfaa2c186aaa5a8c483
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-07-27 10:41:08 -0300

    Hero

--- trilhaapp/lib/pages/card_page.dart
+++ trilhaapp/lib/pages/card_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/model/card_detail.dart';
+import 'package:trilhaapp/pages/card_detail_page.dart';
 
 class CardPage extends StatefulWidget {
   const CardPage({super.key});
@@ -8,6 +10,12 @@ class CardPage extends StatefulWidget {
 }
 
 class _CardPageState extends State<CardPage> {
+  var cardDetail = CardDetail(
+      1,
+      "Meu Card",
+      "https://hermes.digitalinnovation.one/assets/diome/logo.png",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis pellentesque est, sed molestie nibh. Vestibulum mi nisi, malesuada nec mollis facilisis, eleifend quis enim. Quisque in quam nunc. Maecenas ullamcorper, felis vel ullamcorper sollicitudin, dolor lectus rutrum tellus, sed luctus erat magna ut risus. Cras et libero ut sem posuere mollis nec sit amet nibh. Etiam maximus eros sit amet pellentesque lacinia. Aliquam in dui at elit dapibus ultrices sed a neque. Phasellus sed varius ipsum, eu elementum sapien. Maecenas tincidunt nisl velit, vel aliquet purus faucibus quis. Sed sodales mi urna, ut dictum magna viverra nec. Donec euismod justo a ligula pretium lacinia.");
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -15,52 +23,65 @@ class _CardPageState extends State<CardPage> {
         Container(
           margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           width: double.infinity,
-          child: Card(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-            elevation: 12,
-            shadowColor: Colors.red,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+          child: InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => CardDetailPage(
+                            cardDetail: cardDetail,
+                          )));
+            },
+            child: Hero(
+              tag: cardDetail.id,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                elevation: 12,
+                shadowColor: Colors.red,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Image.network(
-                        "https://hermes.digitalinnovation.one/assets/diome/logo.png",
-                        height: 20,
+                      Row(
+                        children: [
+                          Image.network(
+                            cardDetail.url,
+                            height: 20,
+                          ),
+                          Text(
+                            cardDetail.title,
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w700),
+                          ),
+                        ],
                       ),
-                      const Text(
-                        "Meu Card",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w700),
+                      const SizedBox(
+                        height: 10,
                       ),
+                      Text(cardDetail.text,
+                          textAlign: TextAlign.justify,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.normal,
+                          )),
+                      Container(
+                          width: double.infinity,
+                          alignment: Alignment.centerRight,
+                          child: TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Ler Mais...",
+                                style: TextStyle(
+                                    decoration: TextDecoration.underline,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold),
+                              )))
                     ],
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis pellentesque est, sed molestie nibh. Vestibulum mi nisi, malesuada nec mollis facilisis, eleifend quis enim. Quisque in quam nunc. Maecenas ullamcorper, felis vel ullamcorper sollicitudin, dolor lectus rutrum tellus, sed luctus erat magna ut risus. Cras et libero ut sem posuere mollis nec sit amet nibh. Etiam maximus eros sit amet pellentesque lacinia. Aliquam in dui at elit dapibus ultrices sed a neque. Phasellus sed varius ipsum, eu elementum sapien. Maecenas tincidunt nisl velit, vel aliquet purus faucibus quis. Sed sodales mi urna, ut dictum magna viverra nec. Donec euismod justo a ligula pretium lacinia.",
-                      textAlign: TextAlign.justify,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.normal,
-                      )),
-                  Container(
-                      width: double.infinity,
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            "Ler Mais...",
-                            style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold),
-                          )))
-                ],
+                ),
               ),
             ),
           ),
